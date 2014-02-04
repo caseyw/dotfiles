b3d6c0b130a1704292bf818c75a456f71a7b2ead
caseyw <c@caseyw.com>
2014-02-04 13:13:51 -0500

    Add brew cask and some apps

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,4 +16,26 @@ fi
 # Install homebrew packages
 brew install grc coreutils spark ack ctags ffmpeg git mutt siege tidy tmux tofrodos watch wget youtube-dl yuicompressor node
 
+# Tap Cask, to allow us to install GUI's
+brew tap phinze/cask
+
+# Install Cask
+brew install brew-cask
+
+# Install GUI's
+brew cask install acorn
+brew cask install adium
+brew cask install alfred
+brew cask install caffeine
+brew cask install dropbox
+brew cask install filezilla
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install mou
+brew cask install mysql-workbench
+brew cask install phpstorm
+brew cask install skype
+brew cask install sublime-text
+
+
 exit 0
